79f31f07e5a6b1a68a1f55cae1a946d42f27034a
Chris Gardner <chris.gardner@section8games.co.uk>
2019-05-05 06:20:52 +0100

    Add initial tests for Install-GacAssembly

--- Tests/Unit/Install-GacAssembly.Tests.ps1
+++ Tests/Unit/Install-GacAssembly.Tests.ps1
@@ -6,6 +6,18 @@ InModuleScope BizTalkManagement {
 
         Context "Testing inputs" {
 
+            It "Should have an AssemblyName parameter" {
+                (Get-Command $Sut).Parameters['AssemblyName'].Attributes.Mandatory | Should -BeFalse
+            }
+
+            It "Should have an AssemblyLocation parameter" {
+                (Get-Command $Sut).Parameters['AssemblyLocation'].Attributes.Mandatory | Should -BeFalse
+            }
+
+            It "Should throw an exception when AssemblyLocation is invalid" {
+                {&$Sut -AssemblyName 'Test' -AssemblyLocation C:\FakePath.txt} | Should -Throw "Unable to find C:\FakePath.txt"
+            }
+
         }
 
         Context "Testing outputs" {
